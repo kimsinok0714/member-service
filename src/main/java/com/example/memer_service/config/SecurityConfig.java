df19ef5f60935f779a599a31ba808fd98f3d222c
kimsinok0714 <javaenjoy0714@gmail.com>
2025-05-11 11:01:18 +0900

    Update SecurityConfig.java

--- src/main/java/com/example/memer_service/config/SecurityConfig.java
+++ src/main/java/com/example/memer_service/config/SecurityConfig.java
@@ -1,27 +1,27 @@
-package com.example.memer_service.config;
+// package com.example.memer_service.config;
 
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.config.Customizer;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+// import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-@EnableWebSecurity
-//@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig {
+// @Configuration
+// @EnableWebSecurity
+// //@EnableGlobalMethodSecurity(prePostEnabled = true)
+// public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain  filterChain(HttpSecurity http) throws Exception {
+//     @Bean
+//     public SecurityFilterChain  filterChain(HttpSecurity http) throws Exception {
 
-        http
-            .authorizeHttpRequests(auth -> auth
-            .anyRequest().authenticated())
-            .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));            
+//         http
+//             .authorizeHttpRequests(auth -> auth
+//             .anyRequest().authenticated())
+//             .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));            
         
-        return http.build();
-    }
+//         return http.build();
+//     }
 
-}
+// }
